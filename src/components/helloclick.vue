58c88d02af7b958c9f6a0cb0d5e98c4caeba3302
Mateusz Podeszwa <mateuszpodeszwa@icloud.com>
2023-04-22 20:59:37 +0100

    The first real commit

---
+++ src/components/helloclick.vue
@@ -0,0 +1,28 @@
+<!-- src/components/ExampleComponent.vue -->
+<template>
+    <div>
+        <h2>{{ message }}</h2>
+        <button @click="handleButtonClick">Click me!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hi',
+        };
+    },
+    methods: {
+        handleButtonClick() {
+            this.message = 'Hello from Vue Component!';
+        },
+    },
+};
+</script>
+
+<style scoped>
+h2 {
+    color: blue;
+}
+</style>
